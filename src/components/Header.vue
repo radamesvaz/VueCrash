3f3f8ec22d50229939c5c16ef721a554b7fce821
radames vaz <rgvtovar@hotmail.com>
2021-02-25 10:31:15 -0400

    subiendo varios componentes del task

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button color="green" text="Agregar" />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+    name: 'Header',
+    props: {
+        title:{
+            type: String,
+            default: 'Hola, soy Batman'
+        }
+    },
+    components: {
+        Button
+    }
+    
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
