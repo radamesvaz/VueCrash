c3d45405cb0528e6be36a490944a0195f0cfeb16
Radames Vaz <radames.vaz@hulihealth.com>
2021-02-28 19:13:12 -0400

    agregando backend con json-server

--- src/App.vue
+++ src/App.vue
@@ -46,6 +46,11 @@ export default {
     },
     handleToggleForm(){
       this.showAddTask = !this.showAddTask
+    },
+    async fetchTasks(){
+      const res = await fetch('http://localhost:5000/tasks');
+      const data = await res.json();
+      return data
     }
   },
   data() {
@@ -54,27 +59,8 @@ export default {
       showAddTask: false
     };
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: "Doctors Appointment",
-        day: "March 1st at 2:30pm",
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: "Meeting at School",
-        day: "March 3rd at 1:30pm",
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: "Food Shopping",
-        day: "March 3rd at 11:00am",
-        reminder: false,
-      },
-    ];
+  async created() {
+   this.tasks = await this.fetchTasks()
   },
 };
 </script>
